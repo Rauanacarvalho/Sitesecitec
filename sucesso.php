4561b86284265a16a5333127663f45ffb8116334
Rauanacarvalho <92538266+Rauanacarvalho@users.noreply.github.com>
2022-06-08 11:27:08 -0300

    Create sucesso.php

---
+++ sucesso.php
@@ -0,0 +1,20 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+    <h1><center><strong> Formulário PHP </strong></h1>
+      <style>
+         body {
+           background-color: #e6e6ff;
+         }
+      </style>
+  </head>
+  <body>
+   <form action="sucesso.php" method="post">
+      Nome: <input type="text" name="nome_pessoa"><br>
+     </br>
+      E-mail: <input type="text" name="email_pessoa"><br>
+     </br>
+      <input type="submit" value="Enviar">
+    </form>
+  </body>
+</html>
