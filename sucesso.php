c68eb78b0d49cc2b44599d4817a174c8c63ac5ee
Rauanacarvalho <92538266+Rauanacarvalho@users.noreply.github.com>
2022-06-08 11:29:20 -0300

    Create sucesso.php

---
+++ sucesso.php
@@ -0,0 +1,42 @@
+<html>
+  <head>
+  <title>PHP Test</title>
+  <style>
+    body {
+    background-color: #e6e6ff;
+    }
+  </style>
+<body>
+
+    <strong>Seu cadastro foi realizado com sucesso!</strong><br>
+    Nome: <?php echo $_POST["nome_pessoa"]; ?><br>
+    E-mail: <?php echo $_POST["email_pessoa"]; ?>
+
+  <?php
+
+    $nome_pessoa = $_POST['nome_pessoa'];
+    $email_pessoa = $_POST['email_pessoa'];
+    $conexao = new mysqli('localhost','ifpb','ifpb');
+
+    if (!$conexao){
+      die('Não foi possível conectar' .  mysql_error());
+    }
+
+    $conexao->select_db("secitec");
+
+    $query = "INSERT INTO `secitec`.`participante`(`nome`,`email`) VALUES ('" . $nome_pessoa . "','" .  $email_pessoa . "');";
+
+    if($conexao->query($query) === TRUE){
+
+      echo "Cadastrado com sucesso!";
+
+    }else{
+      echo "Erro: " . mysql_error();
+    }
+
+    $conexao->close();
+
+  ?>
+
+</body>
+</html>
