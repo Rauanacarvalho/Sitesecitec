80bddff25e8235df91841be88d44f165b4ec9110
Rauanacarvalho <92538266+Rauanacarvalho@users.noreply.github.com>
2022-07-13 07:31:46 -0400

    Create cadastrar_atividades.php

---
+++ cadastrar_atividades.php
@@ -0,0 +1,24 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+    <h1><center><strong> Formulário PHP </strong></h1>
+      <style>
+         body {
+           background-color: #e6e6ff;
+         }
+      </style>
+  </head>
+  <body>
+   <form action="cadastroatividades.php" method="post">
+      Nome da atividade: <input type="text" name="nome_atividade"><br>
+     </br>
+      Categoria: <input type="text" name="categoria"><br>
+     </br>
+     Data: <input type="text" name="data"><br>
+     </br>
+     Horário: <input type="text" name="horario"><br>
+     </br>
+      <input type="submit" value="Enviar">
+    </form>
+  </body>
+</html>
