b7f00a6ddb0932c308ae7bf30c391c2a1319236f
Rauanacarvalho <92538266+Rauanacarvalho@users.noreply.github.com>
2022-07-16 16:29:24 -0300

    Update cadastro.php

--- cadastro.php
+++ cadastro.php
@@ -1,20 +1,33 @@
+<!DOCTYPE html>
 <html>
   <head>
+     <meta charset="utf-8">
     <title>PHP Test</title>
-    <h1><center><strong> Formulário PHP </strong></h1>
-      <style>
-         body {
-           background-color: #e6e6ff;
-         }
-      </style>
+    <link rel="stylesheet" type="text/css" href="../../estilo/style.css">
   </head>
   <body>
-   <form action="sucesso.php" method="post">
-      Nome: <input type="text" name="nome_pessoa"><br>
-     </br>
-      E-mail: <input type="text" name="email_pessoa"><br>
-     </br>
-      <input type="submit" value="Enviar">
-    </form>
+    <div class="container">
+      <div class="box">
+     <form class="form" action="../sucesso.php" method="post">
+
+      <h1 class="h1"><b> Cadastro </b></h1>
+
+         <label for="nome">Nome:</label><br><br>
+        <input id="nome" type="text" name="nome_pessoa" placeholder="Nome completo" ><br>
+       </br>
+
+       <label for="email">E-mail:</label><br><br>
+        <input id="email" type="email" name="email_pessoa" placeholder="exemplo@academico.ifpb.edu.br" ><br>
+       </br>
+       
+        <input class="cadastrobutton" type="submit" name="cadastrobutton" value="Cadastre-se"><br></br>
+      </form>
+
+      <form class="butaologin" method="post" action="../home/login.html">
+        <button class="login1" name="botao" >Login</button>
+      </form>
+    
+      </div>
+    </div>
   </body>
 </html>
